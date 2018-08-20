736c263db49b44a321bc4543ecbcae7245602670
Luis <enrique.sanabria28@gmail.com>
2018-08-20 17:50:19 -0500

    Ajuste de SISACT se agrego campo de estatus a los metodos restantes y de cotizador se ajustaron la seccion de evaluación

--- src/aura/COT_EvaluationClient_cmp/COT_EvaluationClient_cmpController.js
+++ src/aura/COT_EvaluationClient_cmp/COT_EvaluationClient_cmpController.js
@@ -2,4 +2,14 @@
 	changeValue : function (component, event, helper) {
     component.set("v.myBool", true);
   },
+
+  	loadOptions: function (component, event, helper) {
+		var opts = [
+		{'label': 'Grupo', 'value': 'Grupo'},
+		{'label': 'Razón Social', 'value': 'Razón Social'},
+		{'label': 'Cuenta Nacional', 'value': 'Cuenta Nacional'},
+		{'label': 'Cuenta Padre', 'value': 'Cuenta Padre'}
+		];
+		component.set("v.options", opts);
+	},
 })
